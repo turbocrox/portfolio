c1f094b030da4f6d14d1aeef84622be509ac28e2
turbocrox <adityapundir502@gmail.com>
2025-05-26 12:26:04 +0530

    asd

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    visualizer({ open: true }), // opens bundle analysis after build
-  ],
+  plugins: [react(), tailwindcss()],
   base: '/portfolio/',
-
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('three')) return 'three';
-            if (id.includes('framer-motion')) return 'framer-motion';
-            if (id.includes('gsap')) return 'gsap';
-            if (id.includes('react')) return 'react-vendor';
-            return 'vendor';
-          }
-        },
-      },
-    },
-  },
-});
+})
